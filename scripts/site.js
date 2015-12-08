88a00f9515deeb9b6cba0751d3a655122c5186a2
Josh Levinger <josh@levinger.net>
2015-12-07 17:00:58 -0800

    use bootstrap scrollspy

--- scripts/site.js
+++ scripts/site.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
+    // mobile nav toggle
     $('nav a#bars').on('click', function() {
         $('nav ul.sidebar').toggle();
+    });
+
+    $('body').scrollspy({
+        target: 'nav'
     });
 });
