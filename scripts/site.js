6272d029c8b9f31264e687375feb11f23f1a66e1
Josh Levinger <josh@levinger.net>
2015-12-07 18:30:23 -0800

    nav smooth scroll, hide when done

--- scripts/site.js
+++ scripts/site.js
@@ -1,7 +1,36 @@
 $(document).ready(function() {
-    // mobile nav toggle
+    // nav toggle
     $('nav a#toggle').on('click', function() {
-        $('nav ul.nav').toggleClass('visible');
+        $('nav ul.nav').slideToggle(function complete() {
+            if ($(this).css('display') === 'block') {
+                $('a#toggle').html("&#8964;"); // chevron down
+            } else {
+                $(this).removeClass('visible');
+                $('a#toggle').html("&#8801;"); // triple bars
+            }
+        }).addClass('visible');
+    });
+
+    $('nav ul.nav li').on('click', function(event) {
+        event.preventDefault();
+
+        // animate scroll to top
+        var href = $(this).children('a').attr('href');
+        var element = $(href);
+        var paddingTop = 20;
+        $('html, body').animate({
+            scrollTop: element.offset().top - element.outerHeight() - paddingTop
+        }, 1000);
+
+        // hide nav if visible
+        $('nav ul.nav').slideToggle().removeClass('visible');
+        // reset content to triple bars
+        $('a#toggle').html("&#8801;");
+    });
+
+    // if resize, hide mobile nav
+    $(window).resize(function() {
+        $('nav ul.nav').removeClass('visible');
     });
 
     $('body').scrollspy({
